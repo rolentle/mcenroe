2bfbe963a46de28a8acba48edc506a6baf600782
Rolen Le <rolentle@gmail.com>
2014-01-31 21:47:30 -0700

    implemented twitter login as done in railscast

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Mcenroe::Application.routes.draw do
+  match "/auth/:provider/callback" => "sessions#create"
+  match "/signout" => "sessions#destroy", :as => :signout
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
