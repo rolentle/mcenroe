78713dfd3a2c6322d5dfa3987595e216f7d08360
Rolen Le <rolentle@gmail.com>
2014-01-31 22:03:06 -0700

    fixed routes to match get commands

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Mcenroe::Application.routes.draw do
-  match "/auth/:provider/callback" => "sessions#create"
-  match "/signout" => "sessions#destroy", :as => :signout
+  get "/auth/:provider/callback" => "sessions#create"
+  get "/signout" => "sessions#destroy", :as => :signout
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
