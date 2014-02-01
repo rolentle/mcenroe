0af6f22d3b8b44a6adcf4117b7709245bc028c80
Rolen Le <rolentle@gmail.com>
2014-02-01 11:00:24 -0700

    reconfiged env vars to work with heroku

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,10 @@
+if Rails.env.production?
+  twitter_key = ENV["TWITTER_CONSUMER_KEY"]
+  twitter_secret = ENV["TWITTER_CONSUMER_SECRET"]
+else
+  twitter_key = Figaro.env.twitter_consumer_key
+  twitter_secret = Figaro.env.twitter_consumer_secret
+end
 Rails.application.config.middleware.use OmniAuth::Builder do
-    provider :twitter, Figaro.env.twitter_consumer_key, Figaro.env.twitter_consumer_secret
+    provider :twitter, twitter_key, twitter_secret
 end
