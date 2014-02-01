ac0beea6fb863756c8bff216f9ad49881a518469
Rolen Le <rolentle@gmail.com>
2014-01-31 21:40:59 -0700

    added figaro, rspec, and omniauth-twitter

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mcenroe::Application.config.secret_key_base = '52e6e0b2d20d73ab74d6de37ac76cb54ef7eca91fb9eee61cb6c5c8cbe549c4b25e3c0913cf67a5cc5f7bc46476a7648790133e06ae3091ff234ebb902c1acac'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Mcenroe::Application.config.session_store :cookie_store, key: '_mcenroe_session'
