726e3bba8265de1382d50f986fbc1a8cb85d5eef
Rolen Le <rolentle@gmail.com>
2014-02-01 11:10:26 -0700

    uninstalled figaro and put keys in bash

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,10 +1,5 @@
-if Rails.env.production?
-  twitter_key = ENV["TWITTER_CONSUMER_KEY"]
-  twitter_secret = ENV["TWITTER_CONSUMER_SECRET"]
-else
-  twitter_key = Figaro.env.twitter_consumer_key
-  twitter_secret = Figaro.env.twitter_consumer_secret
-end
+twitter_key = ENV["TWITTER_CONSUMER_KEY"]
+twitter_secret = ENV["TWITTER_CONSUMER_SECRET"]
 Rails.application.config.middleware.use OmniAuth::Builder do
-    provider :twitter, twitter_key, twitter_secret
+  provider :twitter, twitter_key, twitter_secret
 end
