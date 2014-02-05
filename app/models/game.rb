bd39f531ddd6f7e42e86aaa200f1ce8f2502dd37
Rolen Le <rolentle@gmail.com>
2014-02-04 19:09:14 -0700

    added losers to games

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,13 +1,13 @@
 class Game < ActiveRecord::Base
   has_many :players
   has_many :users, through: :players
-  has_one :winner, class_name: "User" do
-    def find_by_winner(winner_id)
-      User.find(winner_id)
-    end
+
+
+  def winner
+    User.find(winner_id)
   end
 
-  def winner=(user)
-    winner_id = user.id
+  def loser
+    (users - [winner]).first
   end
 end
--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -1,5 +1,26 @@
 require 'spec_helper'
 
 describe Game do
-  pending "add some examples to (or delete) #{__FILE__}"
+  auth = OmniAuth.config.mock_auth[:twitter]
+  let(:user) {User.from_omniauth(auth)}
+
+  it "has a winner" do
+    game = Game.create
+
+    game.users << user
+    game.update_attributes(winner_id: user.id)
+    game.save
+
+    expect(game.winner).to eq user
+  end
+
+  it "has a loser" do
+    game = Game.create
+    user2 = User.create
+    game.users << user
+    game.update_attributes(winner_id: user2.id)
+
+    expect(game.loser).to eq user
+    expect(game.winner).not_to eq user
+  end
 end
